af26186b8da49bcbdca32d2d2dc8c2a25184b499
SONG1178 <40830224+SONG1178@users.noreply.github.com>
2019-06-05 21:02:30 +0100

    Update CsiNet_train.py

--- CsiNet_train.py
+++ CsiNet_train.py
@@ -1,7 +1,8 @@
 import tensorflow as tf
-from keras.layers import Input, Dense, BatchNormalization, Reshape, Conv2D, add, LeakyReLU
-from keras.models import Model
-from keras.callbacks import TensorBoard, Callback
+import tensorflow_probability as tfp
+from tf.keras.layers import Input, Dense, BatchNormalization, Reshape, Conv2D, add, LeakyReLU
+from tf.keras.models import Model
+from tf.keras.callbacks import TensorBoard, Callback
 import scipy.io as sio 
 import numpy as np
 import math
@@ -19,29 +20,98 @@ residual_num = 2
 encoded_dim = 512  #compress rate=1/4->dim.=512, compress rate=1/16->dim.=128, compress rate=1/32->dim.=64, compress rate=1/64->dim.=32
 
 # Bulid the autoencoder model of CsiNet
+
+# change the position of channel in x_train, to indicate the real/imag
+
 def residual_network(x, residual_num, encoded_dim):
-    def add_common_layers(y):
-        y = BatchNormalization()(y)
+    
+    def weight_variable(shape, scale=0.1):
+        # first half of the weight is the real part and the second part is the imaginary part
+        # original input shape [batch_size, height, width, depth]
+        # new input shape [part, batch_size, height, width, depth] where part is used to distinguish real and imaginary part
+  
+  
+        magnitude = tf.convert_to_tensor(tf.cast(tfp.math.random_rayleigh(scale=scale, shape=shape), dtype=tf.float32))
+        phase = tf.random_uniform(tf.convert_to_tensor(shape), minval=-pi, maxval=pi)
+        #initial is the initial weights, part=0 refers to the real part, part=1 refers to the imaginary part
+        out_real = tf.multiply(magnitude, tf.cos(phase))
+        out_imag = tf.multiply(magnitude, tf.sin(phase))
+  
+        initial = tf.stack([out_real, out_imag])
+        return initial
+    
+    def complex_conv(x, out_channel, filter_size, stride=1,name="conv"):
+        with tf.variable_scope(name):
+            # number of channel in the input x
+            in_channel = x.get_shape().as_list()[-1] #get_shape returns a tuple and needed to be converted to a list
+            # shape of the weight and bias
+            shape = [filter_size, filter_size, in_channel, out_channel]
+            # create weight variable
+            sigma = 1/np.sqrt(filter_size**2*(in_channel+out_channel))
+            initial = weight_variable(shape, scale=sigma)
+            w = tf.get_variable('w', initializer = initial)
+            # create bias variable
+            b = tf.get_variable('biases', [out_channel], initializer=tf.constant_initializer(0.0))
+  
+            strides = [1, stride, stride, 1]
+  
+            output_real = tf.nn.conv2d(x[0,:], w[0,:], strides, "SAME") - tf.nn.conv2d(x[1,:], w[1,:], strides, "SAME")
+            output_imag = tf.nn.conv2d(x[0,:], w[1,:], strides, "SAME") + tf.nn.conv2d(x[1,:], w[0,:], strides, "SAME")
+            conv = tf.stack([output_real, output_imag])
+  
+        return tf.nn.bias_add(conv,b)
+    
+    
+    def com_full_layer(x, neurons,name="dense"):
+        with tf.variable_scope(name):
+        
+            sigma = 1/np.sqrt(np.prod(x.get_shape().as_list()[2:]))
+            initial = weight_variable([x.get_shape().as_list()[2],neurons], scale=sigma)
+            w = tf.get_variable('w', initializer = initial)
+            b = tf.get_variable('b', [2,x.get_shape().as_list()[1],neurons],initializer=tf.constant_initializer(0.))
+  
+            out_real = tf.matmul(x[0,:],w[0,:]) - tf.matmul(x[1,:],w[1,:])
+            out_imag = tf.matmul(x[0,:],w[1,:]) + tf.matmul(x[1,:],w[0,:])
+  
+        return tf.stack([out_real, out_imag])+b
+    
+    def complex_BN(x,name='BN'):
+        with tf.variable_scope(name):
+            half_channel = x.get_shape()[0]
+            
+            gamma_rr = tf.get_variable(name='gamma_rr',initializer=tf.convert_to_tensor(1/np.sqrt(2)))
+            gamma_ii = tf.get_variable(name='gamma_rr',initializer=tf.convert_to_tensor(1/np.sqrt(2)))
+            gamma_ri = tf.get_variable(name='gamma_rr',initializer=tf.convert_to_tensor(0))
+            
+            x_real = gamma_rr*x[0:half_channel,:]+gamma_ri*x[half_channel:,:]
+            x_imag = gamma_ri*x[0:half_channel,:]+gamma_ii*x[half_channel:,:]
+            com_x = tf.concat([x_real,x_imag],axis=0)
+            b = tf.get_variable('bias',shape=com_x.get_shape(),initializer=tf.constant_initializer(0.))
+            return com_x+b
+    
+    def add_common_layers(y,name='common_layer'):
+        y = complex_BN(y,name)
         y = LeakyReLU()(y)
         return y
     def residual_block_decoded(y):
         shortcut = y
         y = Conv2D(8, kernel_size=(3, 3), padding='same', data_format='channels_first')(y)
-        y = add_common_layers(y)
+        y = add_common_layers(y,'l_1')
         
         y = Conv2D(16, kernel_size=(3, 3), padding='same', data_format='channels_first')(y)
-        y = add_common_layers(y)
+        y = add_common_layers(y,'l_2')
         
         y = Conv2D(2, kernel_size=(3, 3), padding='same', data_format='channels_first')(y)
-        y = BatchNormalization()(y)
+        y = complex_BN(y)
 
         y = add([shortcut, y])
         y = LeakyReLU()(y)
 
         return y
     
-    x = Conv2D(2, (3, 3), padding='same', data_format="channels_first")(x)
-    x = add_common_layers(x)
+    #x = Conv2D(2, (3, 3), padding='same', data_format="channels_first")(x)
+    x = complex_conv(x, 2, 3)
+    x = add_common_layers(x,'l_in')
     
     
     x = Reshape((img_total,))(x)
