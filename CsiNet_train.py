6e21e60c73c612ef94d510d6f5bada752d1600d7
SONG1178 <40830224+SONG1178@users.noreply.github.com>
2019-06-09 15:51:20 +0100

    Update CsiNet_train.py

--- CsiNet_train.py
+++ CsiNet_train.py
@@ -58,7 +58,7 @@ def residual_network(x, residual_num, encoded_dim):
             output_real = tf.nn.conv2d(xr, wr, strides, "SAME", data_format='NCHW') - tf.nn.conv2d(xi, wi, strides, "SAME", data_format='NCHW')
             output_imag = tf.nn.conv2d(xr, wi, strides, "SAME", data_format='NCHW') + tf.nn.conv2d(xi, wr, strides, "SAME", data_format='NCHW')
             
-            b_real = tf.get_variable('biases_real', shape=output_real.get_shape().as_list(), initializer=tf.constant_initializer(0.0))
+            b_real = tf.get_variable('biases_real', initializer=tf.constant(0.0,shape=output_real.get_shape().as_list()))
             b_imag = tf.get_variable('biases_imag', shape=output_real.get_shape().as_list(), initializer=tf.constant_initializer(0.0))
             
             conv_real = output_real + b_real
