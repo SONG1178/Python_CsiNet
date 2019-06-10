1607edc57253ef2547e3794ffc9d328be945433f
SONG1178 <40830224+SONG1178@users.noreply.github.com>
2019-06-11 00:48:46 +0100

    Update CsiNet_train.py

--- CsiNet_train.py
+++ CsiNet_train.py
@@ -82,10 +82,8 @@ def residual_network(x, residual_num, encoded_dim):
             out_real = subtract([dot(xr,wr),dot(xi,wi)])
             out_imag = add([dot(xr,wi),dot(xi,wr)])
             
-            print(out_real.get_shape())
-            (channel,width,height) = out_real.get_shape().as_list()[1:]
             batch_size = 200
-            shape = [batch_size,channel,width,height]
+            shape = [batch_size,neurons]
             
             b_real = zeros(shape)
             b_imag = zeros(shape)
@@ -103,7 +101,7 @@ def residual_network(x, residual_num, encoded_dim):
             x_real = add([gamma_rr*xr,gamma_ri*xi])
             x_imag = add([gamma_ri*xr,gamma_ii*xi])
             
-            (channel,width,height) = x_real.get_shape().as_list()[1:]
+            channel,width,height = x_real.get_shape().as_list()[1:]
             batch_size = 200
             shape = [batch_size,channel,width,height]
             
